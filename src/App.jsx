3e040a429b53deee6cbc1a2ab85b9211fb87140b
Noah Betzen <noah@nezteb.net>
2022-09-22 13:12:59 -0700

    Typo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countdown from './Countdown'
 function App() {
   return (
     <div className="inline-flex flex-wrap">
-      <Countdown text="MWF2 Launch" targetDate={new Date("Oct 28, 2022")}/>
+      <Countdown text="MW2 Launch" targetDate={new Date("Oct 28, 2022")}/>
       <Countdown text="Games Agnostic Goal" targetDate={new Date("Nov 1, 2022")}/>
       <Countdown text="WZ2 Launch" targetDate={new Date("Nov 16, 2022")}/>
     </div>
