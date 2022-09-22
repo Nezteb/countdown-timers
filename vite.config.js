e59fc601decf590e3d98c6df544e81bf241f48e4
Noah Betzen <noah@nezteb.net>
2022-09-22 13:37:55 -0700

    vite gh pages fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://vitejs.dev/guide/static-deploy.html#github-pages
+  base: "/countdown-timers/",
   plugins: [react()]
 })
